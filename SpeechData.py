06aaacd35e9a472ca91d831e0aec43e2662441fc
KayReid <goalieonfire@gmail.com>
2019-04-22 13:51:55 -0400

    Notes and classifer to use
    Nothing important

--- SpeechData.py
+++ SpeechData.py
@@ -1,1 +1,13 @@
-# Is this connecting to my current repo?
+# import data set
+from sklearn.naive_bayes import MultinomialNB # look up documentation on sklearn
+
+
+# look up documentation to check if there is a simple way to set this up
+x = None # this is our data
+y = None # the correct classifier, either Clinton or Trump
+
+# make a train-test split
+
+# train and fit
+
+# estimate accuracy
