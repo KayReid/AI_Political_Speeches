66eec82df02a6982c6ca00b8a9209ce1d38575c9
raiddydrake <40771936+raiddydrake@users.noreply.github.com>
2019-04-29 13:46:12 -0400

    Add files via upload

---
+++ dataclean.py
@@ -0,0 +1,49 @@
+
+inclinton = ["Clinton_2016-07-28.txt", "Clinton_2016-07-29.txt", "Clinton_2016-08-01.txt"]
+intrump = ["Trump_2016-07-22.txt", "Trump_2016-07-25.txt", "Trump_2016-07-26.txt"]
+outclinton = open("clinton.txt", "w+")
+outtrump = open("trump.txt", "w+")
+delete_list = ["hillary", "clinton", "donald", "trump", "mike",
+               "pence", "bill", "chelsea", "barack", "obama", "joe",
+               "biden", "michelle", "bernie", "sanders", "ted", "cruz",
+               "gary", "johnson", "jill", "stein", "lincoln", "chafee",
+               "lawrence", "lessig", "martin", "o'malley", "webb", "jeb",
+               "bush", "ben", "carson", "chris", "christie", "carly",
+               "fiorina" "jim", "gilmore", "lindsey", "graham", "mike",
+               "huckabee", "bobby", "jindal", "john", "kasich", "george",
+               "pataki", "rand", "paul", "rick", "perry", "santorum",
+               "marco", "rubio", "scott", "walker", "andrew", "cuomo",
+               "al", "gore", "dennis", "kucinich", "brian", "schweitzer",
+               "elizabeth", "warren", "kelly", "ayotte", "nikki", "haley",
+               "peter", "king", "susana", "martinez", "mitt", "romney",
+               "anthony", "weiner", "huma", "abedin", "ryan", "brian",
+               "sandoval", "corey", "lewandowski", "manafort", "ivanka",
+               "tiffany", "barron", "melania", "eric", "steve", "bannon",
+               "kellyanne", "conway", "jeff", "sessions", "david", "bossie",
+               "michael", "glassner", "jason", "miller", "katrina", "pierson",
+               "jason", "miller", "katrina", "pierson", "hope", "hicks",
+               "daniel", "scavino", "jr.", "jr", "flynn", "omarosa", "manigault",
+               "rudy", "giuliani", "palatucci", "cohen", "arthur", "culvahouse",
+               "brad", "parscale", "sam", "nunberg", "gates", "caputo",
+               "vladimir", "putin", "democratic", "republican", "democrat",
+               "republican"]
+for f in inclinton:
+    infile = open(f)
+    for line in infile:
+        a = line.lower()
+        for word in delete_list:
+            b = a.replace(word, "")
+            a = b
+        outclinton.write(b)
+    infile.close()
+outclinton.close()
+for f in intrump:
+    infile = open(f)
+    for line in infile:
+        a = line.lower()
+        for word in delete_list:
+            b = a.replace(word, "")
+            a = b
+        outtrump.write(b)
+    infile.close()
+outclinton.close()
