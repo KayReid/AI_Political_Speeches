c45d7dd4cbe45717edc8bc45d2866e86ead40ee3
KayReid <goalieonfire@gmail.com>
2019-05-06 10:33:46 -0400

    Delete Process and add MNB

--- Processing.py
+++ Processing.py
@@ -3,9 +3,13 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 import csv
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 
 # https://www.analyticsvidhya.com/blog/2018/02/the-different-methods-deal-text-data-predictive-python/
-
+# https://sigdelta.com/blog/text-analysis-in-pandas/
 
 def word_generator(textfile: str):
 
@@ -27,7 +31,7 @@ def word_generator(textfile: str):
 
 
 # split into sets of 100
-def csv_writer(candidate : str, textfile: str, csvfile: str):
+def csv_writer(candidate, textfile: str, csvfile: str):
     # creates a word generator
     generator = word_generator(textfile)
 
@@ -58,14 +62,14 @@ def csv_reader(filename : str):
 
 def vectorize(data):
     vec = CountVectorizer()
-
-    ft = vec.fit_transform(data['data']) #.toarray()
+    ft = vec.fit_transform(data['data'])
 
     # https: // scikit - learn.org / stable / tutorial / text_analytics / working_with_text_data.html
     tfidf_transformer = TfidfTransformer()
     ft_transformer = tfidf_transformer.fit_transform(ft)
 
     return ft_transformer
+
 
 def combine(clinton_data, trump_data):
     frames = [clinton_data, trump_data]
@@ -76,6 +80,7 @@ def combine(clinton_data, trump_data):
 
 if __name__ == "__main__":
 
+    # speakers = {'clinton': 0, 'trump': 1}
     csv_writer('clinton', 'test.txt', 'test.csv')
     csv_writer('trump', 'test.txt', 'test2.csv')
     train = csv_reader('test.csv')
@@ -84,10 +89,30 @@ if __name__ == "__main__":
     speech_data = combine(train, train2)
 
     # experiment with word count
-    train['word_count'] = train['data'].apply(lambda x: len(str(x).split(" ")))
-    train[['data', 'word_count']].head()
-
+    # train['word_count'] = train['data'].apply(lambda x: len(str(x).split(" ")))
+    # train[['data', 'word_count']].head()
     # train['data'] = train['data'].apply(lambda x: TextBlob(x).words)
 
-    vect = vectorize(speech_data)
-    print(vect.shape)
+    # train and test split
+    train, test = train_test_split(speech_data, test_size=0.2)
+
+    test_vec = vectorize(test)
+    train_vec = vectorize(train)
+
+    """
+    frequency_vec = vectorize(speech_data)
+    print(frequency_vec.shape)
+    print(frequency_vec)
+    clf = MultinomialNB().fit(frequency_vec, speech_data['candidate'])
+    """
+
+    # Train a Naive Bayes classifier
+    clf = MultinomialNB()
+    clf.fit(train_vec, train['candidate'])
+
+    # Estimate its accuracy
+    print(test_vec.shape)
+    print(test['candidate'].shape)
+    print(clf.score(test_vec, test['candidate']))
+
+
