ce6f3dcca3f08881d7d2e317a41a06e5b7aaa940
KayReid <goalieonfire@gmail.com>
2019-05-09 19:18:28 -0400

    Working test-train split

--- Processing.py
+++ Processing.py
@@ -63,11 +63,10 @@ def vectorize(data):
     # count words in sample
     vec = CountVectorizer()
     ft = vec.fit_transform(data)
-
     # https: // scikit - learn.org / stable / tutorial / text_analytics / working_with_text_data.html
     # turn into frequency analysis
     transformer = TfidfTransformer()
-    return transformer.fit_transform(ft)
+    return vec, transformer.fit_transform(ft)
 
 
 def combine(dataOne, dataTwo):
@@ -79,6 +78,7 @@ def combine(dataOne, dataTwo):
 
     for datum in dataTwo:
         data.append(datum)
+
     return np.array(data)
 
 
@@ -92,21 +92,41 @@ if __name__ == "__main__":
     trump_x = data_writer(1, 'test.txt')
     trump_y = generate_y(1, trump_x)
 
-    speeches_x = combine(clinton_x, trump_y)
+    speeches_x = combine(clinton_x, trump_x)
     print(len(clinton_x))
     print(len(trump_x))
     speeches_y = combine(clinton_y, trump_y)
-    print(type(speeches_x))
 
-    vec = vectorize(speeches_x)
+    # do frequency analysis on text
+    vec, vect = vectorize(speeches_x)
+
+    # split data
+    train_x = speeches_x[::2] # np.arange(0, len(speeches_x), 2)
+    test_x = speeches_x[1::2] # np.arange(1, len(speeches_x), 2)
+
+    train_y = speeches_y[::2] # np.arange(0, len(speeches_y), 2)
+    test_y = speeches_y[1::2] # np.arange(1, len(speeches_y), 2)
+
+    # TODO: vectorize before making an np array
+    vec_train = vectorize(train_x)
+    vec_test = vectorize(test_x)
 
     # Train a Naive Bayes classifier
     clf = MultinomialNB()
-    clf.fit(vec, speeches_x)
+    clf.fit(vect, speeches_y)
 
     # Estimate its accuracy
-    print(vec.shape)
-    print(speeches_y.shape)
-    print(clf.score(vec, speeches_y))
+    print(vect.shape)
+    test_x = vec.transform(test_x)
+    print(test_x.shape)
+    # test_x = test_x.reshape(-1,1)
+    print(clf.predict(test_x))
+
+    """
+    twenty_test = fetch_20newsgroups(subset='test', categories=categories, shuffle=True, random_state=42)
+    docs_test = twenty_test.data
+    predicted = text_clf.predict(docs_test)
+    np.mean(predicted == twenty_test.target) 
+    """
 
 
